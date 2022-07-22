041db21d810f58a6113a6d62941def9bf323bfa4
Arnaud Christ <arnaud.christ@gmail.com>
2022-07-22 23:47:36 +0200

    Create Tuple type and utility functions.

--- src/tuple.test.ts
+++ src/tuple.test.ts
@@ -1,7 +1,47 @@
+import { Tuple, getTupleType, TupleTypes, createPoint, createVector } from "./tuple"
+
 describe("tuple", () => {
     test("tuple should be a point", () => {
-        const foobar = "foobar"
+        const tuple: Tuple = { x: 4.3, y: -4.2, z: 3.1, w: 1.0 }
 
-        expect(foobar).toEqual("foobar");
+        expect(tuple.x).toEqual(4.3);
+        expect(tuple.y).toEqual(-4.2);
+        expect(tuple.z).toEqual(3.1);
+        expect(tuple.w).toEqual(1.0);
+        expect(getTupleType(tuple)).toEqual(TupleTypes.Point)
+        expect(getTupleType(tuple)).not.toEqual(TupleTypes.Vector)
+    })
+
+    test("tuple should be a vector", () => {
+        const tuple: Tuple = { x: 4.3, y: -4.2, z: 3.1, w: 0.0 }
+
+        expect(tuple.x).toEqual(4.3);
+        expect(tuple.y).toEqual(-4.2);
+        expect(tuple.z).toEqual(3.1);
+        expect(tuple.w).toEqual(0.0);
+        expect(getTupleType(tuple)).not.toEqual(TupleTypes.Point)
+        expect(getTupleType(tuple)).toEqual(TupleTypes.Vector)
+    })
+
+    test("should create a point", () => {
+        const point: Tuple = createPoint(1.1, 2.2, 3.3);
+
+        expect(point.x).toEqual(1.1);
+        expect(point.y).toEqual(2.2);
+        expect(point.z).toEqual(3.3);
+        expect(point.w).toEqual(1.0);
+        expect(getTupleType(point)).toEqual(TupleTypes.Point);
+        expect(getTupleType(point)).not.toEqual(TupleTypes.Vector);
+    })
+
+    test("should create a vector", () => {
+        const vector: Tuple = createVector(1.1, 2.2, 3.3);
+
+        expect(vector.x).toEqual(1.1);
+        expect(vector.y).toEqual(2.2);
+        expect(vector.z).toEqual(3.3);
+        expect(vector.w).toEqual(0.0);
+        expect(getTupleType(vector)).toEqual(TupleTypes.Vector);
+        expect(getTupleType(vector)).not.toEqual(TupleTypes.Point);
     })
 })
--- src/tuple.ts
+++ src/tuple.ts
@@ -1,1 +1,23 @@
-export type Tuple = [number, number, number, number];
+export enum TupleTypes { 
+    Point = "POINT",
+    Vector = "VECTOR"
+}
+
+export type Tuple = { 
+    x: number,
+    y: number,
+    z: number,
+    w: 0 | 1 
+}
+
+export const getTupleType = ( tuple: Tuple ): TupleTypes => {
+    return tuple.w === 0 ? TupleTypes.Vector : TupleTypes.Point;
+}
+
+export const createPoint = ( x: number, y: number, z: number): Tuple => {
+    return { x, y, z, w: 1.0 }
+}
+
+export const createVector = ( x: number, y: number, z: number): Tuple => {
+    return { x, y, z, w: 0.0 }
+}
