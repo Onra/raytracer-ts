8cc81069a8042b2bc51fa4815c36405b0ee0c22c
Arnaud Christ <arnaud.christ@gmail.com>
2022-07-23 16:56:14 +0200

    Add support for prettier.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import { Tuple } from "./tuple"
 
-const foobar = (tuple: Tuple) => {
-    console.log(tuple)
-}
 
-console.log(foobar([1,2,3,1]));
--- src/tuple.ts
+++ src/tuple.ts
@@ -1,58 +1,80 @@
 const EPSILON = 0.00001;
 
-export enum TupleTypes { 
-    Point = 1,
-    Vector = 0
+export enum TupleTypes {
+  Point = 1,
+  Vector = 0,
 }
 
-export type Tuple = { 
-    x: number,
-    y: number,
-    z: number,
-    w: TupleTypes
-}
+export type Tuple = {
+  x: number;
+  y: number;
+  z: number;
+  w: TupleTypes;
+};
 
-export const getTupleType = ( tuple: Tuple ): TupleTypes => {
-    return tuple.w === 0 ? TupleTypes.Vector : TupleTypes.Point;
-}
+export const getTupleType = (tuple: Tuple): TupleTypes => {
+  return tuple.w === 0 ? TupleTypes.Vector : TupleTypes.Point;
+};
 
-export const createPoint = ( x: number, y: number, z: number): Tuple => {
-    return { x, y, z, w: 1.0 }
-}
+export const createPoint = (x: number, y: number, z: number): Tuple => {
+  return { x, y, z, w: 1.0 };
+};
 
-export const createVector = ( x: number, y: number, z: number): Tuple => {
-    return { x, y, z, w: 0.0 }
-}
+export const createVector = (x: number, y: number, z: number): Tuple => {
+  return { x, y, z, w: 0.0 };
+};
 
 export const equalNumbers = (number1: number, number2: number): boolean => {
-    return Math.abs(number1 - number2) < EPSILON;
-}
+  return Math.abs(number1 - number2) < EPSILON;
+};
 
 export const equalTuples = (tuple1: Tuple, tuple2: Tuple): boolean => {
-   return equalNumbers(tuple1.x, tuple2.x) &&
-       equalNumbers(tuple1.y, tuple2.y) &&
-       equalNumbers(tuple1.z, tuple2.z) &&
-       equalNumbers(tuple1.w, tuple2.w);
-}
+  return (
+    equalNumbers(tuple1.x, tuple2.x) &&
+    equalNumbers(tuple1.y, tuple2.y) &&
+    equalNumbers(tuple1.z, tuple2.z) &&
+    equalNumbers(tuple1.w, tuple2.w)
+  );
+};
 
 export const addTuples = (tuple1: Tuple, tuple2: Tuple): Tuple => {
-    return { x: tuple1.x + tuple2.x, y: tuple1.y + tuple2.y, z: tuple1.z + tuple2.z, w: tuple1.w + tuple2.w };
-}
+  return {
+    x: tuple1.x + tuple2.x,
+    y: tuple1.y + tuple2.y,
+    z: tuple1.z + tuple2.z,
+    w: tuple1.w + tuple2.w,
+  };
+};
 
 export const subtractTuples = (tuple1: Tuple, tuple2: Tuple): Tuple => {
-    return { x: tuple1.x - tuple2.x, y: tuple1.y - tuple2.y, z: tuple1.z - tuple2.z, w: tuple1.w - tuple2.w };
-}
+  return {
+    x: tuple1.x - tuple2.x,
+    y: tuple1.y - tuple2.y,
+    z: tuple1.z - tuple2.z,
+    w: tuple1.w - tuple2.w,
+  };
+};
 
 export const negateTuple = (tuple: Tuple): Tuple => {
-    const zeroTuple = { x: 0, y: 0, z: 0, w: TupleTypes.Vector };
+  const zeroTuple = { x: 0, y: 0, z: 0, w: TupleTypes.Vector };
 
-    return subtractTuples(zeroTuple, tuple);
-}
+  return subtractTuples(zeroTuple, tuple);
+};
 
 export const multiplyTuple = (tuple: Tuple, scalar: number): Tuple => {
-    return { x: tuple.x * scalar, y: tuple.y * scalar, z: tuple.z * scalar, w: tuple.w * scalar };
-}
+  return {
+    x: tuple.x * scalar,
+    y: tuple.y * scalar,
+    z: tuple.z * scalar,
+    w: tuple.w * scalar,
+  };
+};
 
 export const divideTuple = (tuple: Tuple, scalar: number): Tuple => {
-    return { x: tuple.x / scalar, y: tuple.y / scalar, z: tuple.z / scalar, w: tuple.w / scalar };
-}
+  return {
+    x: tuple.x / scalar,
+    y: tuple.y / scalar,
+    z: tuple.z / scalar,
+    w: tuple.w / scalar,
+  };
+};
