ca7940774fb83da8045d315536335c9d78c35592
Arnaud Christ <arnaud.christ@gmail.com>
2022-08-06 22:47:52 +0200

    Enhance function to get PPM string with pixels.

--- src/canvas.test.ts
+++ src/canvas.test.ts
@@ -1,9 +1,15 @@
-import { Canvas, createCanvas, writePixel, toPPMString } from "./canvas";
+import {
+  Canvas,
+  createCanvas,
+  writePixel,
+  toPPMString,
+  pixelAt,
+} from "./canvas";
 import { createColor, equalColors, Color } from "./color";
 
 describe("Canvas", () => {
   test("should create a canvas of 10x20 full of black colors", () => {
-    const canvas: Canvas = createCanvas(10, 20);
+    const canvas: Canvas = createCanvas(10, 20, 255);
 
     expect(canvas.width).toEqual(10);
     expect(canvas.height).toEqual(20);
@@ -11,27 +17,39 @@ describe("Canvas", () => {
     for (var i: number = 0; i < 10; i++) {
       for (var j: number = 0; j < 20; j++) {
         expect(
-          equalColors(canvas.pixels[i][j], createColor(0, 0, 0))
+          equalColors(pixelAt(canvas, i, j), createColor(0, 0, 0))
         ).toBeTruthy();
       }
     }
   });
 
   test("should write a pixel on the canvas", () => {
-    const canvas: Canvas = createCanvas(10, 20);
+    const canvas: Canvas = createCanvas(10, 20, 255);
 
     const red: Color = createColor(1, 0, 0);
 
     writePixel(canvas, 2, 3, red);
 
-    expect(equalColors(canvas.pixels[2][3], createColor(1, 0, 0))).toBeTruthy();
+    expect(
+      equalColors(pixelAt(canvas, 2, 3), createColor(1, 0, 0))
+    ).toBeTruthy();
   });
 
   test("should generate PPM string from canvas", () => {
-    const canvas: Canvas = createCanvas(5, 3);
+    const canvas: Canvas = createCanvas(5, 3, 255);
+
+    const color1: Color = createColor(1.5, 0, 0);
+    const color2: Color = createColor(0, 0.5, 0);
+    const color3: Color = createColor(-0.5, 0, 1);
+
+    writePixel(canvas, 0, 0, color1);
+    writePixel(canvas, 2, 1, color2);
+    writePixel(canvas, 4, 2, color3);
 
     const ppm = toPPMString(canvas);
 
-    expect(ppm).toEqual("P3\r5 3\r255");
+    expect(ppm).toEqual(
+      "P3\n5 3\n255\n255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"
+    );
   });
 });
--- src/canvas.ts
+++ src/canvas.ts
@@ -4,24 +4,59 @@ export type Canvas = {
   width: number;
   height: number;
   pixels: Array<Array<Color>>;
+  maxColorValue: number;
 };
 
-export const createCanvas = (width: number, height: number): Canvas => {
+export const createCanvas = (
+  width: number,
+  height: number,
+  maxColorValue: number
+): Canvas => {
   let pixels = [];
 
-  for (var i: number = 0; i < width; i++) {
+  for (var i: number = 0; i < height; i++) {
     pixels.push([]);
-    for (var j: number = 0; j < height; j++) {
+    for (var j: number = 0; j < width; j++) {
       pixels[i].push(createColor(0, 0, 0));
     }
   }
-  return { width, height, pixels };
+  return { width, height, pixels, maxColorValue };
 };
 
-export const writePixel = (canvas: Canvas, x, y, color: Color) => {
-  canvas.pixels[x][y] = color;
+export const writePixel = (
+  canvas: Canvas,
+  x: number,
+  y: number,
+  color: Color
+) => {
+  canvas.pixels[y][x] = color;
+};
+
+export const pixelAt = (canvas: Canvas, x: number, y: number): Color => {
+  return canvas.pixels[y][x];
+};
+
+const sanitizePixelColor = (pixelColor: number): number => {
+  return Math.max(Math.min(pixelColor, 1), 0);
 };
 
 export const toPPMString = (canvas: Canvas): string => {
-  return `P3\r${canvas.width} ${canvas.height}\r255`;
+  let ppm: string = `P3\n${canvas.width} ${canvas.height}\n${canvas.maxColorValue}`;
+
+  for (var i = 0; i < canvas.height; i++) {
+    ppm += "\n";
+    for (var j = 0; j < canvas.width; j++) {
+      const pixel: Color = pixelAt(canvas, j, i);
+
+      ppm += `${Math.round(
+        canvas.maxColorValue * sanitizePixelColor(pixel.red)
+      )} ${Math.round(
+        canvas.maxColorValue * sanitizePixelColor(pixel.green)
+      )} ${Math.round(canvas.maxColorValue * sanitizePixelColor(pixel.blue))}${
+        j !== canvas.width - 1 ? " " : ""
+      }`;
+    }
+  }
+
+  return ppm;
 };
